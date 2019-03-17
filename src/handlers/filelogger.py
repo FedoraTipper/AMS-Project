cfa9ed6919bdce383f9eef09d50f738d04ab31d1
FedoraTipper <FedoraTipper@users.noreply.github.com>
2019-03-17 17:31:27 +0200

    remove logs file

--- src/handlers/filelogger.py
+++ src/handlers/filelogger.py
@@ -4,7 +4,7 @@ import handlers.config as ConfigHandlers
 #Cfg imports seem to break logging's ability to write messages
 #log_cfg_list = ConfigHandlers.get_keys("FILELOGGING")
 
-logging.basicConfig(filename="logs.txt", level=logging.DEBUG, 
+logging.basicConfig(filename="app.log", level=logging.DEBUG, 
 					format='%(asctime)s - %(levelname)s - %(message)s', 
 					filemode='a')
 
